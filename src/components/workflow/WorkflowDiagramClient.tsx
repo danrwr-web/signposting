cf273246950e4715af06e41bf8efa873fd3f239c
Daniel Webber-Rookes <dan.rwr@gmail.com>
2025-12-12 16:04:56 +0000

    Fix: add missing Link import from next/link

--- src/components/workflow/WorkflowDiagramClient.tsx
+++ src/components/workflow/WorkflowDiagramClient.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useCallback, useMemo, useEffect, useRef, startTransition } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import ReactFlow, {
   Node,
   Edge,
