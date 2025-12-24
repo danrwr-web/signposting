cd9a8050be692cbaa58730cc7fff3a4063ab465b
Daniel Webber-Rookes <dan.rwr@gmail.com>
2025-12-24 11:56:50 +0000

    Remove Runner / Instances (beta) link from Admin Tools section

--- src/app/s/[id]/workflow/page.tsx
+++ src/app/s/[id]/workflow/page.tsx
@@ -268,12 +268,6 @@ export default async function WorkflowDashboardPage({ params }: WorkflowDashboar
                 >
                   Manage Templates
                 </Link>
-                <Link
-                  href={`/s/${surgeryId}/workflow/start`}
-                  className="text-sm text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 rounded-md px-3 py-1.5 transition-colors"
-                >
-                  Runner / Instances (beta)
-                </Link>
               </div>
             </section>
           )}
